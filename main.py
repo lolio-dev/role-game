cca9f48454fee9be1f6f549dce2ce153d5b34743
Elie Pernet <elie.pernet@orange.fr>
2021-02-03 21:29:47 +0100

    restart system and colorization of the message no more potion

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class game:
     def __init__(self):
         self.foe = foe(self)
         self.player = player(self)
+        self.running = True
 
     def round(self):
         ask = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) ? ")
@@ -21,22 +22,30 @@ class game:
             if game.player.nb_potion > 0:
                 self.player.heal()
             else:
-                print("plus de potion")
+                print(f"{Fore.RED}plus de potion !!!")
 
         self.foe.attack()
 
+    def restart(self):
+        ask_restart = input("Voulez vous rejouer ? oui(o) non(n)")
+        if ask_restart == "o":
+            self.player.health = 50
+            self.player.nb_potion = 3
+            self.foe.health = 50
+        elif ask_restart == 'n':
+            self.running = False
+
 
 game = game()
-running = True
 
-while running:
+while game.running:
     if game.player.health > 0 and game.foe.health > 0:
         game.round()
 
     elif game.player.health <= 0:
         print(f"{Fore.RED}Désolé vous avez perdu")
-        running = False
+        game.restart()
 
     elif game.foe.health <= 0:
         print(f"{Fore.GREEN}👏 bravo vous avez gagné ")
-        running = False
+        game.restart()
